9e92eb8becad7b67643183d061a05202f9272579
itisem <emily@itisem.com>
2023-09-11 11:53:17 +0200

    suggestion page done

--- app/submit/client-page.tsx
+++ app/submit/client-page.tsx
@@ -2,14 +2,44 @@
 
 import styles from "../../styles/submit.module.css";
 import {useState} from "react";
+import getYoutubeID from "get-youtube-id";
+import supabase from "../../lib/supabase";
 
 export default function Submit(){
 	const [clueType, setClueType] = useState("audio");
 	const [language, setLanguage] = useState("");
 	const [clue, setClue] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [errored, setErrored] = useState(false);
+
+	const error = (message) => {
+		setErrored(true);
+		setErrorMessage(message);
+	}
 
 	const submit = () => {
-		console.log(clueType, language, clue);
+		setErrorMessage("");
+		setErrored(false);
+		const realClue = clueType === "audio" ? getYoutubeID(clue) : clue;
+		// clue type should always be specified. adding this sanity check, but it should never ever occur
+		if(!clueType) return error("error: no clue type specified. this was not meant to happen");
+		if(!language) return error("error: no language specified.");
+		// getYoutubeID returns null if it's not a youtube url, and an id if it is
+		if(realClue === null) return error("error: audio link is not a youtube video url.");
+		// empty string means text clue w/o a value
+		if(realClue === "") return error("error: unspecified text clue.");
+
+		supabase.from("suggestions").insert({
+			language,
+			clue: clueType,
+			value: realClue
+		}).then(() =>{
+			setErrorMessage(`successfully submitted a clue for the ${language} language.`);
+			setClue("");
+			setLanguage("");
+		}).catch(
+			() => error("database error while submitting, please try again later.")
+		)
 	}
 
 	return <div className = {styles.page}>
@@ -33,6 +63,7 @@ export default function Submit(){
 				type = "text" id = "language"
 				placeholder = "language" className = {styles.text}
 				onChange = {e => setLanguage(e.target.value)}
+				value = {language}
 			/>
 		</p>
 		<p>
@@ -40,6 +71,7 @@ export default function Submit(){
 				id = "clue" rows = {5} cols = {40}
 				placeholder = "enter your clue here" className = {styles.text}
 				onChange = {e => setClue(e.target.value)}
+				value = {clue}
 			>
 			</textarea>
 		</p>
@@ -50,6 +82,9 @@ export default function Submit(){
 		</p>
 		<p>
 			if you are submitting an audio clue, please submit a youtube video url!
+		</p>
+		<p className = {errored ? styles.error : styles.feedback}>
+			{errorMessage}
 		</p>
 	</div>
 }
